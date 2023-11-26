0ce10d4bb9e1267a753243dbcd47ccfc2b6bbfaf
Guilherme Gomes <148831147+DevGuilhermeg@users.noreply.github.com>
2023-11-26 18:26:00 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,88 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, redirect
+import requests
+from tkinter import messagebox
 
 app = Flask(__name__)
 
 
+url_pessoas = 'https://api-usuarios-tvdy.onrender.com/pessoas'
+# URL da API para login
+url_login = 'https://api-usuarios-tvdy.onrender.com/login'
+url_cadastrar = 'https://api-usuarios-tvdy.onrender.com/pessoas'
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    # Obtenha o email do parâmetro na URL
+    email = request.args.get('email')
+    dados_usuario = None
+    nome = "Recrutador"
+    
+    # Se o email estiver presente, faça a solicitação à API para obter dados do usuário, se necessário
+    if email:
+        urlEmail = f'https://api-usuarios-tvdy.onrender.com/pessoas/email/{email}'
+        resposta = requests.get(urlEmail)
 
+        if resposta.status_code == 200:
+            # Aqui você pode usar os dados do usuário obtidos da API
+            dados_usuario = resposta.json()
+            nome = (dados_usuario['pessoa']['nome'])
+
+            # ...
+    
+    # Restante do código da rota /index
+    
+    saldaçao = f"Olá, {nome}! Guilherme aqui"
+    print(saldaçao)
+    
+    return render_template('index.html', nome=saldaçao)
 
 @app.route('/login')
 def login():
     return render_template('login.html')
 
+@app.route('/fazer-login', methods=['POST'])
+def fazer_login():
+    # Obter os valores do formulário
+    email = request.form.get('email')
+    senha = request.form.get('senha')
+
+    # Dados de login em formato JSON
+    dados_login = {'email': email, 'senha': senha}
+
+    # Fazendo a requisição POST para o endpoint de login com os dados em JSON
+    resposta = requests.post(url_login, json=dados_login, verify=True)  # O parâmetro 'verify=True' verifica o certificado SSL
+
+    # Verificando o status da resposta
+    if resposta.status_code == 200:
+        # Redirecionar para a página principal ou outra página após o login bem-sucedido
+        return redirect(f'/?email={email}')
+    else:
+        return f"Falha no login. Código de status: {resposta.status_code}"
+    
 
 @app.route('/cadastro')
 def cadastro():
     return render_template('cadastro.html')
 
+@app.route('/cadastrar', methods=['POST'])
+def cadastrar():
+    # Obter os valores do formulário
+    nome = request.form.get('nameC')
+    email = request.form.get('emailC')
+    idade = request.form.get("idadeC")
+    senha = request.form.get('senhaC')
+
+    nome= "giuss"
+    email = 'Guiiii@gmail.com'
+    idade = 28
+    senha = "123"
+
+    dados = {"nome": nome, "email": email, "idade": int(idade), "senha": senha}
+    try:
+        resposta = requests.post(url_cadastrar, json=dados, verify=True)
+
+    except Exception as e:
+        print(f"Erro durante a requisição POST: {str(e)}")
 if __name__ == '__main__':
     app.run(debug=True)
